e6aea2eb6ee176f3d5f5f410a76ec1d978e7ebfa
Ramoonus <Ramoonus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-04-24 07:55:35 +0000

    version 1.0.1 containing a jQuery UI update
    git-svn-id: https://plugins.svn.wordpress.org/jquery-updater/trunk@376488 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- jquery-updater.php
+++ jquery-updater.php
@@ -3,7 +3,7 @@
 Plugin Name: jQuery and jQuery UI updater
 Plugin URI: http://www.ramoonus.nl/wordpress/jquery-updater/
 Description: This plugin should only be used for development purposes, this plugin updates jQuery and jQuery UI to the latest stable version.
-Version: 1.0.0
+Version: 1.0.1
 Author: Ramoonus
 Author URI: http://www.ramoonus.nl
 License: GPL2
@@ -19,15 +19,17 @@ function rw_jquery_update() {
 		wp_enqueue_script('jquery'); // load
 	}
 }
+// load
 add_action('init', 'rw_jquery_update');
 
 // Update jQuery UI
 function rw_jquery_ui_update() { // when not in admin screen
 	if (!is_admin()) { // deregister 
 		wp_deregister_script('jquery-ui');
-		wp_register_script('jquery-ui', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.11/jquery-ui.min.js', false, '1.8.11'); // re register
+		wp_register_script('jquery-ui', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.12/jquery-ui.min.js', false, '1.8.12'); // re register
 		wp_enqueue_script('jquery-ui'); // load
 	}
 }
+// load
 add_action('init', 'rw_jquery_ui_update');
 ?>

b0176d14efc9916ea65bdd013cbe480b9500fbc7
Ramoonus <Ramoonus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-03 08:33:35 +0000

    v1.8.2 in trunk
    git-svn-id: https://plugins.svn.wordpress.org/jquery-updater/trunk@620625 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- jquery-updater.php
+++ jquery-updater.php
@@ -1,25 +1,28 @@
 <?php
 /*
-Plugin Name: jQuery updater
+Plugin Name: jQuery Updater
 Plugin URI: http://www.ramoonus.nl/wordpress/jquery-updater/
-Description: This plugin updates jQuery to the latest (stable) version.
-Version: 1.8.0
+Description: This plugin updates jQuery to the latest  stable version.
+Version: 1.8.2
 Author: Ramoonus
 Author URI: http://www.ramoonus.nl/
 License: GPL2
 */
 
 function rw_jquery_update() {
-		wp_deregister_script('jquery'); 
-		wp_enqueue_script('jquery', plugins_url('/js/jquery-1.8.0.min.js', __FILE__), false, '1.7.2');	
+	   if ( !is_admin() ) {
+			wp_deregister_script('jquery'); 
+			wp_enqueue_script('jquery', plugins_url('/js/jquery-1.8.2.min.js', __FILE__), false, '1.8.2');	
+	   }
 }
 add_action('init', 'rw_jquery_update');
 
+/* Declare Shortcode */
 function rw_jquery_shortcode( $atts, $content = null ) {
    return '<script> jQuery(function($) { ' 
    . $content . 
    ' }); </script>';
 }
-add_shortcode( 'jquery', 'rw_jquery_shortcode' );
-add_shortcode( 'jq', 'rw_jquery_shortcode' );
+add_shortcode( 'jquery', 'rw_jquery_shortcode' ); // [jquery]
+add_shortcode( 'jq', 'rw_jquery_shortcode' ); // [jq]
 ?>

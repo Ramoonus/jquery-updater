be48dc80000cca82f5ac479823e5e4706be813c1
Remzi Cavdar <ict@remzi.info>
2018-11-01 10:46:06 +0100

    A complete revamp
    A complete revamp of the code to support wp admin backend settings and allow much more flexibility.

--- jquery-updater.php
+++ jquery-updater.php
@@ -1,29 +1,175 @@
 <?php
-/*
-Plugin Name: jQuery Updater
-Plugin URI: http://www.ramoonus.nl/wordpress/jquery-updater/
-Description: This plugin updates jQuery to the latest  stable version.
-Version: 2.1.4
-Author: Ramoonus
-Author URI: http://www.ramoonus.nl/
-License: GPL3
-*/
 
-// Register main function
-function rw_jquery_updater() {
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @wordpress-plugin
+ * Plugin Name:       jQuery Updater
+ * Plugin URI:        https://github.com/Ramoonus/jQuery-Updater
+ * Description:       This plugin updates jQuery to the latest stable version and let you manage jQuery and jQuery Migrate.
+ * Version:           4.0.0
+ * Author:            Ramoonus and Remzi Cavdar
+ * Author URI:        https://github.com/Ramoonus/jQuery-Updater
+ * License:           GPL 3.0
+ * License URI:       https://www.gnu.org/licenses/gpl-3.0.en.html
+ */
 
-		// jQuery
-		// Deregister core jQuery
-		wp_deregister_script('jquery');
-	// Register
-		wp_enqueue_script('jquery', plugins_url('/js/jquery-2.1.4.min.js', __FILE__), false, '2.1.4');
+// If this file is called directly, abort.
+defined('ABSPATH') or exit();
 
-		// jQuery Migrate
-		// @since 2.0.0
-		// Deregister core jQuery Migrate
-		wp_deregister_script('jquery-migrate');
-		// Register
-		wp_enqueue_script('jquery-migrate', plugins_url('/js/jquery-migrate-1.2.1.min.js', __FILE__), array('jquery'), '1.2.1'); // require jquery, as loaded above	
+// WPJ_UPDATER_PLUGIN - stands for: WordPress jQuery Updater plugin
+// Config - CONSTANTS
+// http://php.net/manual/en/dir.constants.php & https://www.quora.com/Should-class-constants-be-all-uppercase-in-PHP
+define( 'WPJ_UPDATER_PLUGIN_DIR_PATH', str_replace( "\\", "/", plugin_dir_path(__FILE__) ) );
+define( 'WPJ_UPDATER_PLUGIN_DIR_URL', plugin_dir_url( __FILE__ ) );
+define( 'WPJ_UPDATER_PLUGIN_SITE_URL', get_site_url() );
+define( 'WPJ_UPDATER_PLUGIN_DOMAIN_NAME', $_SERVER['HTTP_HOST'] );
+// Default jQuery version (users are able to set their own jQuery version in the wp admin)
+define( 'WPJ_UPDATER_PLUGIN_JQUERY', plugins_url('/assets/js/jquery-3.3.1.min.js',__FILE__ ) );
+// Default jQuery Migrate version
+define( 'WPJ_UPDATER_PLUGIN_JQUERY_MIGRATE', plugins_url('/assets/js/jquery-migrate-3.0.1.js',__FILE__ ) );
+
+
+// Upon activation set default options in db
+function wpj_updater_plugin_activation() {
+	do_action( 'wpj_updater_plugin_default_options' );
 }
-// Front-End
-add_action('wp_enqueue_scripts', 'rw_jquery_updater');
+register_activation_hook( __FILE__, 'wpj_updater_plugin_activation' );
+
+// Set default values here
+function wpj_updater_plugin_default_values() {
+	// jQuery and jQuery Migrate default url settings upon activation
+	add_option( 'wpj_updater_jquery_url', WPJ_UPDATER_PLUGIN_JQUERY );
+	add_option( 'wpj_updater_jquery_migrate_url', WPJ_UPDATER_PLUGIN_JQUERY_MIGRATE );
+}
+add_action( 'wpj_updater_plugin_default_options', 'wpj_updater_plugin_default_values' );
+
+
+/*
+ * Create WP jQuery Updater menu item in WordPress admin backend in the Tools section
+ * Remzi Cavdar
+ */
+function wpj_updater_plugin_menu() {
+	add_management_page( 'jQuery Updater', 'jQuery Updater', 'administrator', 'wpj-updater-plugin-settings', 'wpj_updater_plugin_settings' );
+	add_action( 'admin_init', 'wpj_updater_plugin_register_settings' );
+}
+add_action( 'admin_menu', 'wpj_updater_plugin_menu' );
+
+function wpj_updater_plugin_register_settings() {
+	// jQuery and jQuery Migrate settings
+	register_setting( 'wpj_updater_plugin_settings', 'wpj_updater_jquery_url', 'wpj_updater_plugin_validation' );
+	register_setting( 'wpj_updater_plugin_settings', 'wpj_updater_jquery_migrate_url', 'wpj_updater_plugin_validation' );
+}
+
+// Sanitize all options it
+function wpj_updater_plugin_validation($input) {
+	return sanitize_text_field($input);
+}
+
+
+/*
+ * WordPress jQuery Updater plugin class, this class gives us flexiblity for doin options and/or debugging
+ * Remzi Cavdar
+ */
+class wpj_updater_plugin {
+	public $jquery;
+	public $jquery_migrate;
+
+	public function __construct() {
+        // jQuery
+		$this->jquery = get_option( 'wpj_updater_jquery_url', WPJ_UPDATER_PLUGIN_JQUERY );
+        // jQuery Migrate
+		$this->jquery_migrate = get_option( 'wpj_updater_jquery_migrate_url', WPJ_UPDATER_PLUGIN_JQUERY_MIGRATE );
+	}
+
+	public function debug() {
+        echo '<h1>Debug information:</h1>';
+        echo '<strong>Domain name:</strong> ' . AD_PLUGIN_DOMAIN_NAME . '<br>';
+        echo '<strong>URL:</strong> ' . AD_PLUGIN_SITE_URL . '<br>';
+        echo '<strong>Plugin directory path:</strong> ' . WPJ_UPDATER_PLUGIN_DIR_PATH . '<br>';
+        echo '<strong>Plugin URL directory path:</strong> ' . WPJ_UPDATER_PLUGIN_DIR_URL . '<br>';
+        echo '<h2>Plugin settings:</h2>';
+        echo '<strong>jQuery URL:</strong> ' . $this->jquery . '<br>';
+        echo '<strong>jQuery Migrate URL:</strong> ' . $this->jquery_migrate . '<br>';
+	}
+}
+
+// Front-end not excuted in the admin and the customizer (for compatibility reasons)
+// See: https://core.trac.wordpress.org/ticket/45130 and https://core.trac.wordpress.org/ticket/37110
+function wpj_updater_plugin_front_end_scripts() {
+	$wp_admin = is_admin();
+    $wp_customizer = is_customize_preview();
+
+    if ( $wp_admin || $wp_customizer ) {
+        // echo 'We are in the WP Admin or in the WP Customizer';
+    }
+    else {
+        $plugin = new wpj_updater_plugin;
+
+        // Deregister WP core jQuery
+        wp_deregister_script('jquery');
+        // Enqueue jQuery in the head, because some plugins broke when we put it in the footer
+        wp_enqueue_script( 'jquery', $plugin->jquery, array(), null, false );
+
+        // Deregister WP core jQuery Migrate
+        wp_deregister_script('jquery-migrate');
+        // Enqueue jQuery Migrate in the footer
+        wp_enqueue_script( 'jquery-migrate', $plugin->jquery_migrate, array('jquery'), null, true );
+    }
+}
+
+// Back end specific
+// Load only on tools.php?page=wpj-updater-plugin-settings (plugin settings)
+function wpj_updater_plugin_admin_scripts($hook) {
+
+	if( $hook != 'tools_page_wpj-updater-plugin-settings' ) {
+		return;
+	}
+
+	// CSS
+	wp_enqueue_style( 'wpj-updater-admin', plugins_url('/assets/css/admin.css', __FILE__), array(), null );
+
+}
+
+// Register styles and scripts
+add_action( 'wp_enqueue_scripts', 'wpj_updater_plugin_front_end_scripts' );
+add_action( 'admin_enqueue_scripts', 'wpj_updater_plugin_admin_scripts' );
+
+
+// Defer jQuery Migrate, possibliy add option later to defer jquery for users
+function wpj_updater_plugin_add_defer_attribute( $tag, $handle ) {
+	if ( is_admin() || is_customize_preview() ) {
+		return $tag;
+	}
+
+	// Add script handles to the array below
+	$scripts_to_defer = array(
+		'jquery-migrate',
+	);
+
+	foreach ( $scripts_to_defer as $defer_script ) {
+		if ( $defer_script === $handle ) {
+			return str_replace( "src", "defer src", $tag );
+		}
+	}
+
+	return $tag;
+}
+add_filter( 'script_loader_tag', 'wpj_updater_plugin_add_defer_attribute', 10, 2 );
+
+
+// Admin page
+require WPJ_UPDATER_PLUGIN_DIR_PATH . 'admin.php';
+
+// Deactivation
+register_deactivation_hook( __FILE__, 'wpj_updater_plugin_deactivation' );
+
+function wpj_updater_plugin_deactivation() {
+	delete_option( 'wpj_updater_jquery_url' );
+	delete_option( 'wpj_updater_jquery_migrate_url' );
+}
